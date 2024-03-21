e9323166b00da4a915812f7f84589384cecd642b
Harry Pierson <harrypierson@outlook.com>
2024-03-20 17:30:03 -0700

    * add login and register methods (and login test) * update all handlers to be under /api

--- src/operations.test.ts
+++ src/operations.test.ts
@@ -13,6 +13,12 @@ describe("operations-test", () => {
     await testRuntime.destroy();
   });
 
+  test("login", async () => {
+    await testRuntime.invoke(TicketVendor).login('alice', 'password');
+    expect(() => testRuntime.invoke(TicketVendor).login('alice', 'incorrect-password')).rejects.toThrow();
+    expect(() => testRuntime.invoke(TicketVendor).login('zed', 'incorrect-password')).rejects.toThrow();
+  });
+
   /**
    * Test the transaction.
    */
@@ -30,26 +36,44 @@ describe("operations-test", () => {
    */
   test("test-productions-endpoint", async () => {
     const res = await request(testRuntime.getHandlersCallback()).get(
-      "/productions"
+      "/api/productions"
     );
     expect(res.statusCode).toBe(200);
-    expect(res.body).toHaveLength(3);
+    expect(res.body).toStrictEqual([
+      {
+        "id": 1, "name": "Starship Sonata",
+        "description": "An epic space opera musical featuring rival starship captains vying for control of the galaxy, with catchy tunes and futuristic dance numbers."
+      },
+      {
+        "id": 2, "name": "Whisker Wonderland",
+        "description": "A whimsical feline fantasy where cats rule the enchanted land of Meowtopia, facing off against a villainous pack of mischievous mice."
+      },
+      {
+        "id": 3, "name": "Fantasy Factory",
+        "description": "In a magical workshop, toys come to life in this whimsical musical about friendship, imagination, and the power of believing in magic."
+      }
+    ]);
   });
 
   test("test-performances-endpoint", async () => {
     const res = await request(testRuntime.getHandlersCallback()).get(
-      "/performances/1"
+      "/api/performances/1"
     );
     expect(res.statusCode).toBe(200);
-    expect(res.body).toHaveLength(4);
+    expect(res.body).toStrictEqual([
+      { "id": 1, "productionId": 1, "description": "Opening Night", "date": "2025-03-14T07:00:00.000Z", "ticketPrice": "25.00", "ticketCount": 10, "soldTicketCount": 5 },
+      { "id": 2, "productionId": 1, "description": "Saturday Matinee", "date": "2025-03-15T07:00:00.000Z", "ticketPrice": "25.00", "ticketCount": 10, "soldTicketCount": 0 },
+      { "id": 3, "productionId": 1, "description": "Saturaday Night", "date": "2025-03-15T07:00:00.000Z", "ticketPrice": "25.00", "ticketCount": 10, "soldTicketCount": 6 },
+      { "id": 4, "productionId": 1, "description": "Final Performance", "date": "2025-03-16T07:00:00.000Z", "ticketPrice": "25.00", "ticketCount": 10, "soldTicketCount": 6 }
+    ]);
   });
 
   test("test-available-seats-endpoint", async () => {
     const res = await request(testRuntime.getHandlersCallback()).get(
-      "/available-seats/1"
+      "/api/available-seats/1"
     );
     expect(res.statusCode).toBe(200);
-    // expect(res.body).toHaveLength(4);
+    expect(res.body).toStrictEqual({ 1: true, 2: false, 3: true, 4: false, 5: true, 6: false, 7: true, 8: false, 9: true, 10: false });
   });
 });
 
--- src/operations.ts
+++ src/operations.ts
@@ -1,15 +1,10 @@
-import { TransactionContext, Transaction, GetApi, ArgSource, ArgSources } from '@dbos-inc/dbos-sdk';
+import { TransactionContext, Transaction, GetApi, ArgSource, ArgSources, PostApi, WorkflowContext, DBOSResponseError, Workflow } from '@dbos-inc/dbos-sdk';
+import { BcryptCommunicator } from '@dbos-inc/communicator-bcrypt';
 import { Knex } from 'knex';
-
-// The schema of the database table used in this example.
-export interface dbos_hello {
-  name: string;
-  greet_count: number;
-}
 
 interface Customer {
   username: string;
-  email: string;
+  password: string;
 }
 
 interface Production {
@@ -35,7 +30,33 @@ interface Reservation {
 
 export class TicketVendor {
 
-  @GetApi('/productions')
+  @PostApi('/api/login')
+  @Transaction({ readOnly: true })
+  static async login(ctxt: TransactionContext<Knex>, username: string, password: string): Promise<void> {
+    const user = await ctxt.client<Customer>('customers').select("password").where({ username }).first();
+    if (!(user && await BcryptCommunicator.bcryptCompare(password, user.password))) {
+      throw new DBOSResponseError("Invalid username or password", 400);
+    }
+  }
+
+  @PostApi('/api/register')
+  @Workflow()
+  static async register(ctxt: WorkflowContext, username: string, password: string): Promise<void> {
+    const hashedPassword = await ctxt.invoke(BcryptCommunicator).bcryptHash(password, 10);
+    await ctxt.invoke(TicketVendor).saveNewUser(username, hashedPassword);
+  }
+
+  @Transaction()
+  static async saveNewUser(ctxt: TransactionContext<Knex>, username: string, hashedPassword: string): Promise<void> {
+    const user = await ctxt.client<Customer>('customers').select().where({ username }).first();
+    if (user) {
+      throw new DBOSResponseError("Username already exists", 400);
+    }
+    await ctxt.client<Customer>('customers').insert({ username, password: hashedPassword });
+  }
+
+
+  @GetApi('/api/productions')
   @Transaction({ readOnly: true })
   static async getProductions(ctxt: TransactionContext<Knex>): Promise<Production[]> {
     const query = ctxt.client<Production>('productions');
@@ -43,7 +64,7 @@ export class TicketVendor {
     return results;
   }
 
-  @GetApi('/performances/:productionId')
+  @GetApi('/api/performances/:productionId')
   @Transaction({ readOnly: true })
   static async getPerformances(ctxt: TransactionContext<Knex>, @ArgSource(ArgSources.URL) productionId: number) {
     const query = ctxt.client<Performance & { reservationCount: number }>('performances')
@@ -56,15 +77,17 @@ export class TicketVendor {
     return results;
   }
 
-  @GetApi('/available-seats/:performanceId')
+  @GetApi('/api/available-seats/:performanceId')
   @Transaction({ readOnly: true })
   static async getAvailableSeats(ctxt: TransactionContext<Knex>, @ArgSource(ArgSources.URL) performanceId: number) {
+    // get the total ticket count for the performance
     const performance = await ctxt.client<Performance>('performances')
       .select('ticketCount')
       .where('id', performanceId)
       .first();
     if (!performance) { throw new Error('Performance not found'); }
 
+    // get the seat numbers that are already reserved
     const { ticketCount } = performance; 
     const seatSet = new Set<number>();
     const reservations = await ctxt.client<Reservation>('reservations')
@@ -72,15 +95,15 @@ export class TicketVendor {
       .where('performanceId', performanceId);
     for (const { seatNumber } of reservations) {
       if (seatNumber > ticketCount) { throw new Error(`Invalid seat number ${seatNumber} of ${ticketCount}`); }
+      if (seatSet.has(seatNumber)) { throw new Error(`Duplicate seat number ${seatNumber}`); }
       seatSet.add(seatNumber);
     }
 
+    // return a map of seat numbers to availability
     const seatMap = new Map<number, boolean>();
     for (let i = 1; i <= performance.ticketCount; i++) { 
       seatMap.set(i, !seatSet.has(i));
     }
-
     return Object.fromEntries(seatMap);
   }
-
 }
