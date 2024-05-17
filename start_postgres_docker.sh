59f0b5bbc240c234362a5855ebe081d54f027f34
Harry Pierson <harrypierson@outlook.com>
2024-05-17 16:06:54 -0700

    update pg docker script

--- start_postgres_docker.sh
+++ start_postgres_docker.sh
@@ -7,20 +7,21 @@ if [[ -z "${PGPASSWORD}" ]]; then
   exit 1
 fi
 
+container="ticket-db"
+
 # Start Postgres in a local Docker container
-docker run --rm --name=dbos-db --env=POSTGRES_PASSWORD=${PGPASSWORD} --env=PGDATA=/var/lib/postgresql/data --volume=/var/lib/postgresql/data -p 5432:5432 -d postgres:16.1
+docker run --rm --name=$container --env=POSTGRES_PASSWORD=${PGPASSWORD} --env=PGDATA=/var/lib/postgresql/data --volume=/var/lib/postgresql/data -p 5432:5432 -d postgres:16.1
 
 # Wait for PostgreSQL to start
 echo "Waiting for PostgreSQL to start..."
 for i in {1..30}; do
-  if docker exec dbos-db psql -U postgres -c "SELECT 1;" > /dev/null 2>&1; then
+  if docker exec $container psql -U postgres -c "SELECT 1;" > /dev/null 2>&1; then
     echo "PostgreSQL started!"
     break
   fi
   sleep 1
 done
 
-docker exec dbos-db psql -U postgres -c "CREATE DATABASE ticket_vendor;"
 npx dbos migrate
 npm run db:seed
 echo "Database started successfully!"
